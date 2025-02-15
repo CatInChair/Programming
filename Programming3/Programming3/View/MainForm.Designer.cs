9ec50a8ef3fbce41c44ce341531cd421b847e2cd
CatInChair <cat_in_chair@aol.com>
2025-02-16 02:02:53 +0700

    Rectangles logic and GUI completed.

--- Programming3/Programming3/View/MainForm.Designer.cs
+++ Programming3/Programming3/View/MainForm.Designer.cs
@@ -28,13 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage = new System.Windows.Forms.TabPage();
+            this.groupBoxRectangles = new System.Windows.Forms.GroupBox();
+            this.listBoxRectangles = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLength = new System.Windows.Forms.TextBox();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.textBoxColor = new System.Windows.Forms.TextBox();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage.SuspendLayout();
+            this.groupBoxRectangles.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(434, 288);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage
+            // 
+            this.tabPage.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage.Controls.Add(this.groupBoxRectangles);
+            this.tabPage.Location = new System.Drawing.Point(4, 22);
+            this.tabPage.Name = "tabPage";
+            this.tabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage.Size = new System.Drawing.Size(426, 262);
+            this.tabPage.TabIndex = 1;
+            this.tabPage.Text = "Classes";
+            // 
+            // groupBoxRectangles
+            // 
+            this.groupBoxRectangles.AutoSize = true;
+            this.groupBoxRectangles.Controls.Add(this.buttonFind);
+            this.groupBoxRectangles.Controls.Add(this.textBoxColor);
+            this.groupBoxRectangles.Controls.Add(this.labelColor);
+            this.groupBoxRectangles.Controls.Add(this.textBoxWidth);
+            this.groupBoxRectangles.Controls.Add(this.labelWidth);
+            this.groupBoxRectangles.Controls.Add(this.textBoxLength);
+            this.groupBoxRectangles.Controls.Add(this.label1);
+            this.groupBoxRectangles.Controls.Add(this.listBoxRectangles);
+            this.groupBoxRectangles.Location = new System.Drawing.Point(16, 16);
+            this.groupBoxRectangles.Name = "groupBoxRectangles";
+            this.groupBoxRectangles.Size = new System.Drawing.Size(388, 237);
+            this.groupBoxRectangles.TabIndex = 0;
+            this.groupBoxRectangles.TabStop = false;
+            this.groupBoxRectangles.Text = "Rectangles";
+            // 
+            // listBoxRectangles
+            // 
+            this.listBoxRectangles.FormattingEnabled = true;
+            this.listBoxRectangles.Location = new System.Drawing.Point(6, 19);
+            this.listBoxRectangles.Name = "listBoxRectangles";
+            this.listBoxRectangles.Size = new System.Drawing.Size(192, 199);
+            this.listBoxRectangles.TabIndex = 0;
+            this.listBoxRectangles.SelectedIndexChanged += ListBoxRectangles_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Length:";
+            // 
+            // textBoxLength
+            // 
+            this.textBoxLength.Location = new System.Drawing.Point(233, 36);
+            this.textBoxLength.Name = "textBoxLength";
+            this.textBoxLength.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLength.TabIndex = 2;
+            this.textBoxLength.TextChanged += TextBoxLength_TextChanged;
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.Location = new System.Drawing.Point(230, 59);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(38, 13);
+            this.labelWidth.TabIndex = 3;
+            this.labelWidth.Text = "Width:";
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(233, 75);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWidth.TabIndex = 4;
+            this.textBoxWidth.TextChanged += TextBoxWidth_TextChanged;
+            // 
+            // labelColor
+            // 
+            this.labelColor.AutoSize = true;
+            this.labelColor.Location = new System.Drawing.Point(230, 98);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(34, 13);
+            this.labelColor.TabIndex = 5;
+            this.labelColor.Text = "Color:";
+            // 
+            // textBoxColor
+            // 
+            this.textBoxColor.Location = new System.Drawing.Point(233, 114);
+            this.textBoxColor.Name = "textBoxColor";
+            this.textBoxColor.Size = new System.Drawing.Size(100, 20);
+            this.textBoxColor.TabIndex = 6;
+            this.textBoxColor.TextChanged += TextBoxColor_TextChanged;
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(244, 195);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(75, 23);
+            this.buttonFind.TabIndex = 7;
+            this.buttonFind.Text = "FInd";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += buttonFind_Click;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(461, 312);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage.ResumeLayout(false);
+            this.tabPage.PerformLayout();
+            this.groupBoxRectangles.ResumeLayout(false);
+            this.groupBoxRectangles.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage;
+        private System.Windows.Forms.GroupBox groupBoxRectangles;
+        private System.Windows.Forms.ListBox listBoxRectangles;
+        private System.Windows.Forms.TextBox textBoxLength;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.TextBox textBoxColor;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.Label labelWidth;
     }
 }
 
--- Programming3/Programming3/View/MainForm.cs
+++ Programming3/Programming3/View/MainForm.cs
@@ -12,9 +12,84 @@ namespace Programming3
 {
     public partial class MainForm : Form
     {
+        private Model.Rectangle[] _rectangles;
+        private Model.Rectangle _currentRectangle;
+        private string[] _colors = new string[] { "Red", "Yellow", "Orange", "Blue" };
+
         public MainForm()
         {
             InitializeComponent();
+
+            Random random = new Random();
+
+            _rectangles = new Model.Rectangle[]
+            {
+                new Model.Rectangle(random.NextDouble() * 100, random.NextDouble() * 100, _colors[random.Next(0, 4)]),
+                new Model.Rectangle(random.NextDouble() * 100, random.NextDouble() * 100, _colors[random.Next(0, 4)]),
+                new Model.Rectangle(random.NextDouble() * 100, random.NextDouble() * 100, _colors[random.Next(0, 4)]),
+                new Model.Rectangle(random.NextDouble() * 100, random.NextDouble() * 100, _colors[random.Next(0, 4)]),
+                new Model.Rectangle(random.NextDouble() * 100, random.NextDouble() * 100, _colors[random.Next(0, 4)])
+            };
+
+            for (int i = 0; i < _rectangles.Length; i++) 
+            { 
+                this.listBoxRectangles.Items.Add($"Rectangle {i + 1}");
+            }
+
+            this.listBoxRectangles.SelectedIndex = 0;
+        }
+
+        public void ListBoxRectangles_SelectedIndexChanged(object sender, EventArgs arg)
+        {
+            _currentRectangle = _rectangles[this.listBoxRectangles.SelectedIndex];
+
+            this.textBoxLength.Text = _currentRectangle.GetLength().ToString();
+            this.textBoxWidth.Text = _currentRectangle.GetWidth().ToString();
+            this.textBoxColor.Text = _currentRectangle.GetColor();
+        }
+
+        public void TextBoxLength_TextChanged(object sender, EventArgs arg)
+        {
+
+        }
+
+        public void TextBoxWidth_TextChanged(object sender, EventArgs arg)
+        {
+            this.textBoxWidth.BackColor = SystemColors.Window;
+
+            try
+            {
+                _currentRectangle.SetWidth(Double.Parse(this.textBoxWidth.Text));
+            }
+            catch 
+            {
+                this.textBoxWidth.BackColor = Color.LightPink;
+            }
+        }
+
+        public void TextBoxColor_TextChanged(object sender, EventArgs arg)
+        {
+            _currentRectangle.SetColor(this.textBoxColor.Text);
+        }
+
+        private int FindRectangleWithMaxWidth(Model.Rectangle[] rectangles)
+        {
+            int maxIndex = 0;
+
+            for (int i = 1; i < rectangles.Length; i++) 
+            {
+                if (rectangles[i].GetWidth() > rectangles[maxIndex].GetWidth())
+                {
+                    maxIndex = i;
+                }
+            }
+
+            return maxIndex;
+        }
+
+        public void buttonFind_Click(object sender, EventArgs arg)
+        {
+            this.listBoxRectangles.SelectedIndex = FindRectangleWithMaxWidth(_rectangles);
         }
     }
 }
